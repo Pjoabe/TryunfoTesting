14fe5a9ac45766cf6215db1aee5705e5e6dd3bcb
joabe <joabe9999pereira@gmail.com>
2022-12-24 02:19:00 -0300

    requisito 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,67 +2,88 @@ import React, { Component } from 'react';
 
 class Form extends Component {
   render() {
+    // hasTrunfo
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage, cardRare,
+      cardTrunfo, isSaveButtonDisabled, onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <fieldset>
         <form>
           <label htmlFor="name">
             <input
+              value={ cardName }
               id="name"
               name="card-name"
               type="text"
               data-testid="name-input"
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="description">
             <input
+              value={ cardDescription }
               id="description"
               type="text"
               name="description"
               data-testid="description-input"
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="number1">
             <input
+              value={ cardAttr1 }
               id="number1"
               name="number1"
               type="number"
               data-testid="attr1-input"
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="number2">
             <input
+              value={ cardAttr2 }
               id="number2"
               name="number2"
               type="number"
               data-testid="attr2-input"
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="number3">
             <input
+              value={ cardAttr3 }
               id="number3"
               name="number3"
               type="number"
               data-testid="attr3-input"
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="image">
             <input
+              value={ cardImage }
               id="image"
               name="image"
               type="text"
               data-testid="image-input"
+              onChange={ onInputChange }
             />
           </label>
 
           <label htmlFor="select">
             <select
+              value={ cardRare }
               id="select"
               data-testid="rare-input"
+              onChange={ onInputChange }
             >
               <option>normal</option>
               <option>raro</option>
@@ -72,12 +93,16 @@ class Form extends Component {
 
           <label htmlFor="checkbox">
             <input
+              checked={ cardTrunfo }
               data-testid="trunfo-input"
               type="checkbox"
+              onChange={ onInputChange }
             />
           </label>
 
           <button
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
             id="button"
             data-testid="save-button"
             type="button"
@@ -90,5 +115,7 @@ class Form extends Component {
     );
   }
 }
+
+Form.propTypes = {}.isRequired;
 
 export default Form;
