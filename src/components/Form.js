2585847d2affe4d7136c52decffa95ed0f043961
joabe <joabe9999pereira@gmail.com>
2022-12-24 01:56:53 -0300

    requisito 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,90 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <h1>OI eu sou o form :D</h1>
+      <fieldset>
+        <form>
+          <label htmlFor="name">
+            <input
+              id="name"
+              name="card-name"
+              type="text"
+              data-testid="name-input"
+            />
+          </label>
+
+          <label htmlFor="description">
+            <input
+              id="description"
+              type="text"
+              name="description"
+              data-testid="description-input"
+            />
+          </label>
+
+          <label htmlFor="number1">
+            <input
+              id="number1"
+              name="number1"
+              type="number"
+              data-testid="attr1-input"
+            />
+          </label>
+
+          <label htmlFor="number2">
+            <input
+              id="number2"
+              name="number2"
+              type="number"
+              data-testid="attr2-input"
+            />
+          </label>
+
+          <label htmlFor="number3">
+            <input
+              id="number3"
+              name="number3"
+              type="number"
+              data-testid="attr3-input"
+            />
+          </label>
+
+          <label htmlFor="image">
+            <input
+              id="image"
+              name="image"
+              type="text"
+              data-testid="image-input"
+            />
+          </label>
+
+          <label htmlFor="select">
+            <select
+              id="select"
+              data-testid="rare-input"
+            >
+              <option>normal</option>
+              <option>raro</option>
+              <option>muito raro</option>
+            </select>
+          </label>
+
+          <label htmlFor="checkbox">
+            <input
+              data-testid="trunfo-input"
+              type="checkbox"
+            />
+          </label>
+
+          <button
+            id="button"
+            data-testid="save-button"
+            type="button"
+          >
+            Salvar
+          </button>
+
+        </form>
+      </fieldset>
     );
   }
 }
